b2686626161f92149c571a0377f4effe90bde5c3
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-03-04 09:10:11 -0500

    implementing navigation menu

---
+++ src/router/router.js
@@ -0,0 +1,23 @@
+import { viewTemplateSection } from '../view/template-section.js';
+
+const changeTmp = (hash) => {
+  if (hash === '#/' || hash === '' || hash === '#') {
+    return viewTmp('#/home');
+  } else if (hash === '#/acercademi' || hash === '#/portafolio' || hash === '#/skills' || hash === '#/contacto') {
+    return viewTmp(hash);
+  } else {
+    return viewTmp('#/different');
+  }
+};
+
+const viewTmp = (routers) => {
+  const router = routers.substr(2, routers.length - 2);
+  console.log(router);
+  const container = document.getElementById('container');
+  container.innerHTML = viewTemplateSection[router];  
+};
+
+window.addEventListener('load', changeTmp(window.location.hash))
+if (('onhashchange' in window)) window.onhashchange = () => changeTmp(window.location.hash);
+
+
---
+++ src/view/template-section.js
@@ -0,0 +1,76 @@
+export const viewTemplateSection = {
+  acercademi: `
+    <div>
+      <figure>
+        <img src="images/micaela-suarez.JPG" class="foto-perfil">
+      </figure>
+      <div>
+        <p>Hola mi nombre es Micaela Suarez Cortez. Front-end developer, a quien le gusta aprender 
+         de manera autodidacta y trabajar en equipo. 
+         Apasionada por la tecnología debido a las enormes posibilidades y oportunidades que nos
+         brinda para implementar ideas innovadoras que faciliten y aporten al desarrollo de la sociedad. 
+         He desarrollado aplicaciones web responsive con JavaScript, HTML5, CSS3 y Firebase.</p>
+      </div>
+     </div>
+    `,
+  portafolio: `
+    <div>
+      <div>
+        <p>Portafolio</p>
+      </div>
+      <figure>
+        <img src="#" alt="cipher">
+        <p>Cipher</p>
+        <img src="#" alt="pokemon-go">
+        <p>Pokemon Go</p>
+        <img src="#" alt="joinclude">
+        <p>Joinclude</p>
+        <img src="#" alt="vital">
+        <p>Vital</p>
+        <img src="#" alt="paletica">
+        <p>Paletica</p>
+        <img src="#" alt="beneficencia">
+        <p>Beneficencia</p>
+        <img src="#" alt="ica-express">
+        <p>Ica Express</p>
+        <img src="#" alt="social"> 
+        <p>Social</p>     
+      </figure>
+    </div>
+   `,
+  skills: `
+  <div>
+    <div>
+      <p>Skills</p>
+    </div>
+    <figure>
+      <img src="#" alt="javascript">      
+      <img src="#" alt="html5">      
+      <img src="#" alt="css3">    
+      <img src="#" alt="bootstrap">    
+      <img src="#" alt="git/github">   
+      <img src="#" alt="firebase"> 
+      <img src="#" alt="scrum">     
+      <img src="#" alt="uml">     
+    </figure>
+</div>
+  `,
+  contacto: `
+  <div>
+    <div>
+      <p>Contacto</p>
+    </div>
+    <div>
+      <p>¿Interesadx en hacer un proyecto juntos? Contáctame en: </p>
+    </div>
+    <div>
+     <ul>
+       <li><img src="">micaela.suarez.c@gmail.com</li>
+       <li><img src="">975336010</li>
+       <li><img src="">https://www.linkedin.com/in/micaelasuarez/</li>
+     </ul>
+    </div>
+</div>
+  `
+};
+
