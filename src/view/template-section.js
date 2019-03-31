0ce533078611c1a150a567b2ae21a8625effd5d8
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-03-31 18:18:11 -0500

    modificando los nombres de las clases para que sean más descriptivos

--- src/view/template-section.js
+++ src/view/template-section.js
@@ -4,11 +4,10 @@ export const viewTemplateSection = {
   <p>ACERCA DE MÍ</p>
   </div> 
     <div class="container-about-me">      
-       <figure>
+      <figure>
           <img src="images/micaela-suarez.JPG" class="foto-perfil">
-        </figure>
-      
-        <div class = "presentation">
+      </figure>      
+      <div class = "presentation">
           <p class="description-about-me">Hola mi nombre es Micaela Suarez Cortez y soy Front-end developer egresada de Laboratoria, he desarrollado aplicaciones web con HTML5, CSS3, JavaScript y Firebase.</p>
       </div>
    </div>
@@ -20,42 +19,42 @@ export const viewTemplateSection = {
     <div class="container-section">    
       <div>
         <figure>
-        <a href="https://micaelasuarezcortez.github.io/lim-2018-11-bc-core-am-cipher/src/index.html"><img src="images/cipher.png" alt="cipher" class="img-project" ></a>         
+        <a href="https://micaelasuarezcortez.github.io/lim-2018-11-bc-core-am-cipher/src/index.html"><img src="images/cipher.png" alt="cipher" class="image" ></a>         
         </figure>
       </div>
       <div>
         <figure>
-        <a href="https://micaelasuarezcortez.github.io/lim-2018-11-bc-core-am-data-lovers/src/index.html"><img src="images/pokemon.png" alt="pokemon-go" class="img-project"></a>
+        <a href="https://micaelasuarezcortez.github.io/lim-2018-11-bc-core-am-data-lovers/src/index.html"><img src="images/pokemon.png" alt="pokemon-go" class="image"></a>
         </figure>
       </div>
       <div>
         <figure>
-        <a href="https://micaelasuarezcortez.github.io/LIM008-social-network/src/"><img src="images/social-network.png" alt="joinclude" class="img-project"></a>
+        <a href="https://micaelasuarezcortez.github.io/LIM008-social-network/src/"><img src="images/social-network.png" alt="joinclude" class="image"></a>
         </figure>
       </div>
       <div>
         <figure>
-        <a href="https://micaelasuarezcortez.github.io/LIM008-social-network-2/src/"><img src="images/social-network-vital.png" alt="vital" class="img-project"></a>
+        <a href="https://micaelasuarezcortez.github.io/LIM008-social-network-2/src/"><img src="images/social-network-vital.png" alt="vital" class="image"></a>
         </figure>
       </div>
       <div>
         <figure>
-        <a href ="https://micaelasuarezcortez.github.io/paletica/"><img src="images/paletica.png" alt="paletica" class="img-project"></a>        
+        <a href ="https://micaelasuarezcortez.github.io/paletica/"><img src="images/paletica.png" alt="paletica" class="image"></a>        
         </figure>
       </div>
       <div> 
       <figure>
-      <a href ="https://micaelasuarezcortez.github.io/Ica-Express/"><img src="images/ica-express.png" alt="ica-express" class="img-project"></a>
+      <a href ="https://micaelasuarezcortez.github.io/Ica-Express/"><img src="images/ica-express.png" alt="ica-express" class="image"></a>
       </figure>
       </div>
       <div>
         <figure>
-        <a href ="https://micaelasuarezcortez.github.io/beneficencia/"><img src="images/beneficencia.png" alt="beneficencia" class="img-project"></a>
+        <a href ="https://micaelasuarezcortez.github.io/beneficencia/"><img src="images/beneficencia.png" alt="beneficencia" class="image"></a>
         </figure>
       </div>     
       <div>
       <figure>
-        <a href="https://marvelapp.com/477fe0i"><img src="images/chicken-love.png" alt="chicken-love" class="img-project"></a>         
+        <a href="https://marvelapp.com/477fe0i"><img src="images/chicken-love.png" alt="chicken-love" class="image"></a>         
       </figure>      
       </div>
     </div>
@@ -67,42 +66,42 @@ export const viewTemplateSection = {
     <div class="container-section">    
       <div>
         <figure>
-        <img src="images/js.png" alt="javaScript" class="img-project"></a>         
+        <img src="images/js.png" alt="javaScript" class="image"></a>         
         </figure>
       </div>
       <div>
         <figure>
-        <img src="images/html5.png" alt="html5" class="img-project"></a>
+        <img src="images/html5.png" alt="html5" class="image"></a>
         </figure>
       </div>
       <div>
         <figure>
-        <img src="images/css3.svg" alt="css3" class="img-project"></a>
+        <img src="images/css3.svg" alt="css3" class="image"></a>
         </figure>
       </div>
       <div>
         <figure>
-        <img src="images/bootstrap.png" alt="vital" class="img-project"></a>
+        <img src="images/bootstrap.png" alt="vital" class="image"></a>
         </figure>
       </div>
       <div>
         <figure>
-        <img src="images/git.jpg" alt="git" class="img-project"></a>        
+        <img src="images/git.jpg" alt="git" class="image"></a>        
         </figure>
       </div>
       <div> 
       <figure>
-      <img src="images/git-hub.jpg" alt="git-hub" class="img-project"></a>
+      <img src="images/git-hub.jpg" alt="git-hub" class="image"></a>
       </figure>
       </div>
       <div>
         <figure>
-        <img src="images/firebase.png" alt="firebase" class="img-project"></a>
+        <img src="images/firebase.png" alt="firebase" class="image"></a>
         </figure>
       </div>     
       <div>
       <figure>
-        <img src="images/scrum.png" alt="scrum" class="img-project"></a>         
+        <img src="images/scrum.png" alt="scrum" class="image"></a>         
       </figure>      
       </div>
     </div>   
@@ -115,22 +114,22 @@ export const viewTemplateSection = {
     <div class="subtitle-contacto">
       <p>¿Interesadx en hacer un proyecto juntos? Contáctame en: </p>
     </div>
-    <div class="contact-information">
+    <div class="contact-data">
       <figure>
       <img src="images/email.png"><span> micaela.suarez.c@gmail.com</span>
       </figure>
     </div>
-    <div class="contact-information">  
+    <div class="contact-data">  
       <figure>    
       <img src="images/phone.png"><span> 975336010</span>
       </figure>
     </div>
-    <div class="contact-information">
+    <div class="contact-data">
       <figure>
         <img src="images/linkedin-boton-circular-social.png"><a href="https://www.linkedin.com/in/micaelasuarez/" class="link"><span> Visita mi Linkedin</span></a>  
       </figure>
     </div>
-    <div class="contact-information">
+    <div class="contact-data">
       <figure>
       <img src="images/portafolio.png"><a href="document/micaela-suarez-cortez.pdf" download="cv-micaela-suarez" class="link"><span> Descarga mi CV</span></a>  
       </figure>
@@ -138,4 +137,3 @@ export const viewTemplateSection = {
   </div>
   `
 };
-
